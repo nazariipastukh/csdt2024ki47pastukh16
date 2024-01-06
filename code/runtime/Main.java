10eedd6db0c6187d84d92583207f7badeab9fefd
nazariipastukh <nazariy2002n@gmail.com>
2024-01-06 14:22:49 +0200

    task4 commit

--- code/runtime/Main.java
+++ code/runtime/Main.java
@@ -5,26 +5,49 @@ import ui.UIFrame;
 
 import javax.swing.*;
 
+/**
+ * @class Main
+ * @brief Main class responsible for initializing and starting the application.
+ * <p>
+ * This class includes the main method to start the application, loading settings,
+ * creating a user interface, and launching a thread for the game logic.
+ */
 public class Main {
-    private static runtime.Settings settings;
     private static UIFrame ui;
-
+    /**
+     * @brief Static method to load and initialize the application settings.
+     * <p>
+     * If no saved game is found in the settings, a new CheckerBoard is created.
+     *
+     * @param args Command-line arguments (unused in this application).
+     */
     public static void main(String[] args) {
-        settings = runtime.Settings.loadSettings();
+        // Load application settings
+        runtime.Settings settings = runtime.Settings.loadSettings();
+
+        // Check if a saved game exists, create a new CheckerBoard if not
         if (settings.getSavedGame() == null) {
             settings.setSaveGame(new CheckerBoard(settings));
         }
-        SwingUtilities.invokeLater(new Runnable(){
 
+        // Create and display the user interface using SwingUtilities.invokeLater
+        SwingUtilities.invokeLater(new Runnable(){
             @Override
             public void run() {
                 ui = new UIFrame(settings);
                 settings.getSavedGame().setUI(Main.access$000().game);
             }
         });
+
+        // Start a new thread for the game logic
         new Thread(settings.getSavedGame()).start();
     }
 
+    /**
+     * @brief Static method to access the UI frame.
+     *
+     * @return The UIFrame instance.
+     */
     static UIFrame access$000() {
         return ui;
     }
